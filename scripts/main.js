17f44b5049cac10ee428b1e971693449a249bed1
Unomars4 <une.nyide@gmail.com>
2023-05-31 20:52:10 +0200

    add basic ui and  grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-drawGrid(16, 16);
+drawGrid(100, 100);
 
 
 function drawGrid(rows, cols) {
     const container = document.querySelector(".draw-container");
     console.log(container.style.width);
-    for (let i = 0; i < (rows * cols); i++) {
-        const block = document.createElement("div");
-        block.classList = `block`;
-        container.appendChild(block);
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const block = document.createElement("div");
+            block.classList = `block`;
+            if ((i + j) % 2 == 0) block.style.backgroundColor = "grey";
+            container.appendChild(block);
+        }
     }
 }
