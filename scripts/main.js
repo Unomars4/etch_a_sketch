1ca003db6c8f66b8887e88ab2ca9c7d9126f83d7
Unomars4 <une.nyide@gmail.com>
2023-06-01 17:00:30 +0200

    add drawGrid, sliderChange, displayDimensions

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,41 @@
-drawGrid(100, 100);
+const slider = document.querySelector(".grid-slider");
+const container = document.querySelector(".draw-container");
 
+drawGrid(20);
+// slider.addEventListener("change", sliderChangeGrid);
+container.addEventListener("onmouseover", updateMousePosition);
 
-function drawGrid(rows, cols) {
-    const container = document.querySelector(".draw-container");
-    console.log(container.style.width);
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
+function drawGrid(gridDimension = 16) {
+    const width = calcBlockDimensions(gridDimension);    
+    
+    for (let i = 0; i < gridDimension; i++) {
+        for (let j = 0; j < gridDimension; j++) {
             const block = document.createElement("div");
             block.classList = `block`;
+            block.style.width = `${width}px`, block.style.height = `${width}px`;
             if ((i + j) % 2 == 0) block.style.backgroundColor = "grey";
             container.appendChild(block);
         }
     }
+
+    container.style.gridTemplateRows =  `repeat(${gridDimension}, 1fr)`;
+    container.style.gridTemplateColumns =  `repeat(${gridDimension}, 1fr)`;
+    showBlockDimensions(gridDimension);
+}
+
+function sliderChangeGrid() {
+    const value = slider.value;
+    showBlockDimensions(value);
+    drawGrid(value);
+}
+
+function calcBlockDimensions(blockDimension) {
+    return container.clientWidth / blockDimension ;
+}
+
+function showBlockDimensions(blockDimension) {
+    const widthDisplay = document.querySelector(".width");
+    const heightDisplay = document.querySelector(".height");
+    widthDisplay.textContent = `${blockDimension}px`;
+    heightDisplay.textContent = `${blockDimension}px`;
 }
